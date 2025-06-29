0fd0e8d12eccb8f72e6b3ad4b08d143dad8540c4
Honey Kumar <honeykumar@codebrewinnovations.com>
2025-06-29 12:45:37 +0530

    update

--- dist/data/countryData.d.ts
+++
@@ -1,9 +0,0 @@
-export interface CountryPhoneData {
-    countryCode: string;
-    countryCode3: string;
-    countryCallingCode: string | number;
-    countryName: string;
-    mobile_begin_with: string[];
-    phone_number_lengths: number[];
-}
-export declare const countryPhoneData: CountryPhoneData[];
